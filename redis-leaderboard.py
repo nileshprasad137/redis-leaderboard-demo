60b660f92fd40eeb87b2ae0f927f5e8bd0b0388c
nileshprasad137 <prasadnilesh96@gmail.com>
2020-12-26 20:42:16 +0530

    learnt to add elemnts to sorted set

--- redis-leaderboard.py
+++ redis-leaderboard.py
@@ -1,21 +1,40 @@
 import redis
 import sys
 import random
-redis_client = redis.Redis()
-try:
-    redis_client.ping()
+redisClient = redis.Redis()
+
+def getRandomUsersAndScores():
     users = []
-    i = 1
-    for i in range(1,10):
+    for i in range(1, 10):
         player = {}
-        player["playerID"] = i
-        player["score"] = random.randint(1,100)
+        player["playerID"] = str(i)
+        player["score"] = random.randint(1, 100)
         users.append(player)
+    return users
+
+
+try:
+    redisClient.ping()
+    users = []
+    users = getRandomUsersAndScores()
+    redis
+    playerSortedSet = "players"
+    # Add the users in Sorted Set
+    for user in users:
+        print(user["playerID"], user["score"] )
+        redisClient.zadd(playerSortedSet, {user["playerID"]:user["score"]})
+    print("Contents of the Redis sorted set in ascending order:")
+
+    print(redisClient.zrange(playerSortedSet, 0, -1, withscores=True))
+
+
+
+
 
 except redis.ConnectionError as connectionException:
     print("Redis is down! Check Redis server")
     print(connectionException)
-except Exception as exception:
-    print(exception)
 
-print(users)
+# print(users)
+
+
